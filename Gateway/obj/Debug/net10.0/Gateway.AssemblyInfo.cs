905e7b09b48c06557c353305d3d95adc76460648
Helouri B. <helouribeucherie@gmail.com>
2026-03-09 11:37:39 +0100

    Fix/Merge error 2

--- Gateway/obj/Debug/net10.0/Gateway.AssemblyInfo.cs
+++ Gateway/obj/Debug/net10.0/Gateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+070c828e83f7226a4d13344402003e40d9dd5071")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e607370a505425974cac85cb125b6894370d5e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
