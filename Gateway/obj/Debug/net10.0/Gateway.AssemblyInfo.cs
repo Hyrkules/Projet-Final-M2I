4bb6c836c9253a73c298fd46bbd9e2eb60c463b7
Karamiel <y.seguillon@gmail.com>
2026-03-11 16:26:31 +0100

    Maj3

--- Gateway/obj/Debug/net10.0/Gateway.AssemblyInfo.cs
+++ Gateway/obj/Debug/net10.0/Gateway.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be027127392fdca535c0dfaa1dd9be56963cafb1")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e93999cf23b229ca366493655041303ced8aae")]
+>>>>>>> 65231f1 (Maj)
 [assembly: System.Reflection.AssemblyProductAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
