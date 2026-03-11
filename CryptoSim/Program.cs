dd5d13ae3312ee57bec80641ee1d826983c4b92d
Karramiel <y.seguillon@gmail.com>
2026-03-11 15:57:59 +0100

    Update Program.cs

--- CryptoSim/Program.cs
+++ CryptoSim/Program.cs
@@ -1,40 +1,33 @@
 using CryptoSim.Blazor.Components;
 using CryptoSim.Blazor.Services;
-// Ajoute Blazored.LocalStorage si tu l'utilises (très recommandé)
-// using Blazored.LocalStorage; 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Services de base
+// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-// 2. Configuration de l'URL Gateway
-// Note : Dans Docker, "gateway" est le nom du service défini dans ton docker-compose
 var gatewayUrl = builder.Configuration["GatewayBaseUrl"] ?? "http://localhost:5005";
 
-builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<CryptoSim.Blazor.Services.NotificationService>();
 
-// 3. OPTIMISATION : Configuration HttpClient
-// Cette ligne permet d'injecter directement HttpClient dans tes services (AuthService, etc.)
-builder.Services.AddHttpClient<AuthService>(client =>
+builder.Services.AddHttpClient("Gateway", client =>
 {
     client.BaseAddress = new Uri(gatewayUrl);
 });
 
-// Si tu as d'autres services, fais de même :
-// builder.Services.AddHttpClient<MarketService>(client => client.BaseAddress = new Uri(gatewayUrl));
+builder.Services.AddScoped(sp =>
+    sp.GetRequiredService<IHttpClientFactory>().CreateClient("Gateway"));
 
-// 4. (Optionnel mais conseillé) Gestion du stockage du Token
-// builder.Services.AddBlazoredLocalStorage(); 
+builder.Services.AddScoped<AuthService>();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
 }
-
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseAntiforgery();
 
@@ -43,3 +36,4 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+ 
