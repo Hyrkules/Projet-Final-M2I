9d634225e7aa3f336cc6e9cab6302ffd94fd2b4e
Helouri B. <helouribeucherie@gmail.com>
2026-03-12 10:42:09 +0100

    Feat/Adding datetime when paiement is refused

--- Gateway/DTOs/OrderResponseDto.cs
+++ Gateway/DTOs/OrderResponseDto.cs
@@ -9,6 +9,8 @@
         public decimal Price { get; set; }
         public decimal Total { get; set; }
         public string Status { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } // ✅ ajouté
+
         public DateTime? ExecutedAt { get; set; }
     }
 }
--- Gateway/obj/Debug/net10.0/Gateway.AssemblyInfo.cs
+++ Gateway/obj/Debug/net10.0/Gateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64764c5eb168852e3dd4f04ff4bee50abfc8bde9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53513f13a5267656b71cbd1abca20dfcb7614fe5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
