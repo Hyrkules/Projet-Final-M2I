b5d39787c3eea3954f198c0ddbdc6e5cbeb64300
Helouri B. <helouribeucherie@gmail.com>
2026-03-12 11:14:47 +0100

    Feat/On peut maintenant changer son mdp et nom de user

--- Gateway/obj/Debug/net10.0/Gateway.AssemblyInfo.cs
+++ Gateway/obj/Debug/net10.0/Gateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53513f13a5267656b71cbd1abca20dfcb7614fe5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7914cb72d47a48760fac7f80e504ed8aadce1b50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
