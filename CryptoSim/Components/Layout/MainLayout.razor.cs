6012ddb02cb8f8a272587666935f82e8a7ace366
Helouri B. <helouribeucherie@gmail.com>
2026-03-13 10:00:16 +0100

    Fix/Removing code from razor pages and add them to class files

---
+++ CryptoSim/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,23 @@
+﻿using CryptoSim.Blazor.Services;
+
+namespace CryptoSim.Blazor.Components.Layout
+{
+    public partial class MainLayout
+    {
+        private bool IsDark = true;
+
+        private void ToggleTheme()
+        {
+            IsDark = !IsDark;
+            StateHasChanged();
+        }
+
+        protected override void OnInitialized()
+        {
+            if (!AuthState.IsAuthenticated)
+                Navigation.NavigateTo("/login");
+
+            Notification.OnShow += (msg) => InvokeAsync(StateHasChanged);
+        }
+    }
+}
