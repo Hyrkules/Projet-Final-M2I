8c03d69e7466962338f955afba739d0fc77d38de
Helouri B. <helouribeucherie@gmail.com>
2026-03-13 13:51:42 +0100

    Fix/Profil correctement relié a l API

--- AuthService/DTOs/AuthResponse.cs
+++ AuthService/DTOs/AuthResponse.cs
@@ -4,8 +4,10 @@
     {
         public string Token { get; set; }
         public string Username { get; set; }
+        public string Email { get; set; }
         public string Role { get; set; }
         public decimal Balance { get; set; }
+        public DateTime CreatedAt { get; set; }
         public long ExpiresIn { get; set; }
     }
 }
--- CryptoSim/Services/AuthResponse.cs
+++ CryptoSim/Services/AuthResponse.cs
@@ -4,7 +4,9 @@
     {
         public string Token { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
         public decimal Balance { get; set; }
     }
 }
